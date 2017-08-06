f0f113a83738098bc71fea5771c0fd75e4d4d407
James Ross <git@james-ross.co.uk>
2017-08-06 15:00:59 +0100

    Add configuration management and command-line option

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using CLP = CommandLineParser;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Accounts_Normaliser
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var config = new CLP.Arguments.FileArgument('c', "config")
+            {
+                DefaultValue = new FileInfo("config.json")
+            };
+
+            var commandLineParser = new CLP.CommandLineParser()
+            {
+                Arguments = {
+                    config,
+                }
+            };
+
+            try
+            {
+                commandLineParser.ParseCommandLine(args);
+
+                Main(new ConfigurationBuilder()
+                    .AddJsonFile(config.Value.FullName, true)
+                    .Build());
+            }
+            catch (CLP.Exceptions.CommandLineException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(IConfigurationRoot config)
         {
             Console.WriteLine("Hello World!");
         }
