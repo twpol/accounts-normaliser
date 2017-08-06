4ad0b55c9c51440e8cb9592bfaed0873ec7389f0
James Ross <git@james-ross.co.uk>
2017-08-06 18:55:51 +0100

    Remove non-numeric characters from numeric fields when reading CSV

--- Formats/Csv.cs
+++ Formats/Csv.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using CsvHelper;
 using Microsoft.Extensions.Configuration;
 
@@ -17,34 +18,34 @@ namespace Accounts_Normaliser.Formats
                 {
                     using (var csv = new CsvReader(reader))
                     {
-            while (csv.Read())
-            {
-                if (csv.GetField(0) == csv.FieldHeaders[0])
-                    continue;
+                        while (csv.Read())
+                        {
+                            if (csv.GetField(0) == csv.FieldHeaders[0])
+                                continue;
 
-                if (account.BankID == null & account.AccountID == null)
-                {
-                    account = new Model.Account(
-                        GetValue(config["BankID"], csv),
-                        GetValue(config["AccountID"], csv),
-                        (Model.AccountType)Enum.Parse(typeof(Model.AccountType), GetValue(config["AccountType"], csv)),
-                        GetValue(config["Currency"], csv)
-                    );
-                }
+                            if (account.BankID == null & account.AccountID == null)
+                            {
+                                account = new Model.Account(
+                                    GetValue(config["BankID"], csv),
+                                    GetValue(config["AccountID"], csv),
+                                    (Model.AccountType)Enum.Parse(typeof(Model.AccountType), GetValue(config["AccountType"], csv)),
+                                    GetValue(config["Currency"], csv)
+                                );
+                            }
 
-                var deposit = ParseDecimal(GetValue(config["Deposit"], csv));
-                var withdrawal = ParseDecimal(GetValue(config["Withdrawal"], csv));
+                            var deposit = ParseDecimal(GetValue(config["Deposit"], csv));
+                            var withdrawal = ParseDecimal(GetValue(config["Withdrawal"], csv));
 
-                if (deposit != 0 && withdrawal != 0)
-                    throw new InvalidDataException($"CSV line {csv.Row} contains non-zero deposit {deposit} and non-zero withdrawal {withdrawal} values");
+                            if (deposit != 0 && withdrawal != 0)
+                                throw new InvalidDataException($"CSV line {csv.Row} contains non-zero deposit {deposit} and non-zero withdrawal {withdrawal} values");
 
-                account.Transactions.Add(new Model.Transaction(
-                    DateTimeOffset.Parse(GetValue(config["Date"], csv)),
-                    deposit - withdrawal,
-                    GetValue(config["Description"], csv),
-                    GetValue(config["Memo"], csv)
-                ));
-            }
+                            account.Transactions.Add(new Model.Transaction(
+                                DateTimeOffset.Parse(GetValue(config["Date"], csv)),
+                                deposit - withdrawal,
+                                GetValue(config["Description"], csv),
+                                GetValue(config["Memo"], csv)
+                            ));
+                        }
                     }
                 }
             }
@@ -63,8 +64,13 @@ namespace Accounts_Normaliser.Formats
             return configValue;
         }
 
+        static char[] NumericCharacters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',' };
+
         static decimal ParseDecimal(string value)
         {
+            while (value.Length > 0 && !NumericCharacters.Contains(value[0]))
+                value = value.Substring(1);
+
             if (value == "")
                 return 0;
 
